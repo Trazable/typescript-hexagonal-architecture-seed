1f5a01722ef717b6422ea5a332a5d860c8d5844e
fran.gimenez <fran.gimenez@trazable.io>
2020-07-21 10:25:50 +0200

    feat(test): add save test

--- __tests__/services/example/example.service.test.js
+++ __tests__/services/example/example.service.test.js
@@ -1,17 +1,36 @@
 const expect = require('expect')
 const ExampleService = require('../../../src/services/example.service')
 
-describe('when get all data', () => {
-  it ('should return all data from the database', () => {
+let dataSource
+before(function () {
+  // DATA SOURCE MOCK
+  dataSource = {
+    getAll: function() { return { name: 'name' }},
+    save: function() { return { name: 'name'}}
+  }
 
-    const dataSource = {
-      getAll: function() { return { name: 'name' }},
-    }
+})
 
-    const exampleService = new ExampleService(dataSource)
-
-    const response = exampleService.getAll()
-
-    expect(response).toStrictEqual({ name: 'name' })
+describe('Example Service tests', () => {
+  describe('when get all data', () => {
+    it ('should return all data from the database', () => {
+  
+      const exampleService = new ExampleService(dataSource)
+  
+      const response = exampleService.getAll()
+  
+      expect(response).toStrictEqual({ name: 'name' })
+    })
   })
+  describe('when save data', () => {
+    it ('should return the data saved in the database', () => {
+  
+      const exampleService = new ExampleService(dataSource)
+  
+      const response = exampleService.save({ name: 'name'})
+  
+      expect(response).toStrictEqual({ name: 'name' })
+    })
+  })
+  
 })
