574fb770c7ab90509bf7451dedd3ccd5566a6ff0
fran.gimenez <fran.gimenez@trazable.io>
2020-10-28 12:15:55 +0100

    feat(entities): remove business entity
    Create the structure of the desired object in the run-time getting the necessary data from the input.

--- src/services/student.service.js
+++ src/services/student.service.js
@@ -1,16 +1,31 @@
-const { StudentEntity } = require('../entities/student.entity')
 
 // STUDENT SERVICE
+const StudentRepository = require("../repositories/student.repository")
+
 // Recogemos la dependencia inyectada desde el controlador
+/**
+ * 
+ * @param {StudentRepository} studentRepository 
+ */
 module.exports = (studentRepository) => {
   /**
    *
    * @param {*} student
+   * @return {Promise<T>}
    */
-  const save = async (student) => {
+  const save = async (studentData) => {
     // Business logic
-    student = new StudentEntity(student)
-    student = { ...student, param: 'modified data' }
+
+    // Creamos la estructura en run-time que sera almacenada por primera vez en el guardado de la base de datos.
+    // En este ejemplo, si studentData no tiene name, age y hobbies lanzara un error de undefined.
+    const student = {
+      name: studentData.name,
+      age: studentData.age,
+      hobbies: studentData.hobbies || [],  
+      createdAt: new Date()
+    }
+    // Si no creamos ningun objeto y solo metemos lo que venga por el input, la base de datos lo guardaría sin ningún problema.
+
     // Llamamos al repositorio para hacer el guardado en base de datos, si este no esta implementado en el dataSource el repositorio lanzara un error por falta de implementación
     return studentRepository.save(student)
   }
