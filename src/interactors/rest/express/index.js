5d4de8747fb5688ee219d066e26f243c1adf957c
Lucas <lucas@trazable.io>
2020-11-05 12:10:45 +0100

    feat(logger): add logger to express

--- src/interactors/rest/express/index.js
+++ src/interactors/rest/express/index.js
@@ -1,13 +1,13 @@
 /*
  * Express configuration as REST interactor
  */
-
 const app = require('express')()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const httpStatusCodes = require('http-status-codes')
 const routes = require('./routes')
 const dependencies = require('../../../dependencies')
+const { loggerController: logger } = require('../../../utils/logger')
 
 // Middlewares
 app.use(bodyParser.json())
@@ -24,8 +24,7 @@ app.use('/', routes(dependencies))
 // Listen for requests
 const port = 8080
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is listening on port ${port}`)
+  logger.info(`Server is listening on port ${port}`)
 })
 
 
