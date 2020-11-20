c63d96584a39d41c700511c0d801ee707740ea03
fran.gimenez <fran.gimenez@trazable.io>
2020-11-20 15:35:18 +0100

    chore(index): initialize all aplication components
    Instantiante and create the necessary componentes in the main file of the application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-/**
- * INITIALIZE ALL INTERACTORS
- */
+// APPLICATION INDEX
 
+/// //// SECONDARY ADAPTERS (OUTPUT) \\\\ \\\
 
-require('./interactors/rest/express')
+// Mongo database configuration
+const MongoManager = require('./adapters/mongo')
+const mongoClient = new MongoManager(process.env.DB_URI, process.env.DB_USER, process.env.DB_PASSWORD).getClient()
+
+// Mongo datasource injectionclear
+const MongoExampleDataSource = require('./adapters/mongo/example.datasource')
+// Repositories
+const exampleRepository = new MongoExampleDataSource(mongoClient)
+
+/// //// PRIMARY PORTS (CORE) \\\\ \\\
+
+// Managers
+const ExampleManager = require('./interactors/example')
+// Dependencies injection
+const exampleManager = new ExampleManager(exampleRepository)
+
+/// //// PRIMARY ADAPTERS (INPUT) \\\\ \\\
+
+// Express configuration
+const ExpressApi = require('./adapters/express')
+// Dependency injection
+const api = new ExpressApi(exampleManager)
+
+api.start(8080)
+
