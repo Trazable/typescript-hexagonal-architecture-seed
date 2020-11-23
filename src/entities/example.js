63675ab24886569b83ac2327be553a831e246557
fran.gimenez <fran.gimenez@trazable.io>
2020-11-23 15:35:33 +0100

    chore(jsdocs): method documentation

--- src/entities/example.js
+++ src/entities/example.js
@@ -3,15 +3,27 @@ class Example {
    *
    * @param {{ id: string, name: string, lastName: string, phone: number, createdAt: Date}}
    */
-  constructor ({ name, lastName, phone, createdAt }) {
+  constructor ({ id, name, lastName, phone, createdAt }) {
+    this.id = id
     this.name = name
     this.lastName = lastName
     this.phone = phone
     this.createdAt = createdAt
   }
 
+  /**
+   * @return {string}
+   */
   get nameAndLastName () {
     return `${this.name} ${this.lastName}`
+  }
+
+  /**
+   *
+   * @param {string} name
+   */
+  changeName (name) {
+    this.name = name
   }
 }
 
