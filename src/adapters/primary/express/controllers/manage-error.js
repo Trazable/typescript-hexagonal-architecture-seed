45ffff87ff9430f79bc8d0f061799f301fa947f9
fran.gimenez <fran.gimenez@trazable.io>
2020-11-24 11:09:08 +0100

    feat(error): create error manager

---
+++ src/adapters/primary/express/controllers/manage-error.js
@@ -0,0 +1,20 @@
+const NameAlreadyExists = require('../../../../exceptions/name-already-exists')
+
+const { BAD_REQUEST, INTERNAL_SERVER_ERROR } = require('http-status-codes')
+const ExampleNotFound = require('../../../../exceptions/example-not-found')
+
+module.exports = (error, res) => {
+  let response
+  switch (error.constructor) {
+    case NameAlreadyExists:
+      response = res.status(BAD_REQUEST).json('The example name already exists')
+      break
+    case ExampleNotFound:
+      response = res.status(BAD_REQUEST).json('Example not found')
+      break
+    default:
+      response = res.status(INTERNAL_SERVER_ERROR).end()
+  }
+
+  return response
+}
