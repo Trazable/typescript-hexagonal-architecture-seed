573c5e0bc85715fbe2bd91f06c065e14478587be
fran.gimenez <fran.gimenez@trazable.io>
2020-11-24 11:17:25 +0100

    feat(logger): initialize and inject logger

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 
 /// //// SECONDARY ADAPTERS (OUTPUT) \\\\ \\\
 
+// Google dependencies
+const GoogleWinstonLogger = require('./adapters/secondary/google/logger.repository')
+const googleContainerLogger = new GoogleWinstonLogger()
+
 // Mongo database configuration
 const MongoManager = require('./adapters/secondary/mongo')
 const mongoClient = new MongoManager(process.env.DB_URI, process.env.DB_USER, process.env.DB_PASSWORD).getClient()
@@ -9,21 +13,30 @@ const mongoClient = new MongoManager(process.env.DB_URI, process.env.DB_USER, pr
 // Mongo datasource injectionclear
 const MongoExampleDataSource = require('./adapters/secondary/mongo/example.datasource')
 // Repositories
-const exampleRepository = new MongoExampleDataSource(mongoClient)
+const exampleRepository = new MongoExampleDataSource(mongoClient, googleContainerLogger.getDataSourceContainer())
 
 /// //// PRIMARY PORTS (CORE) \\\\ \\\
 
 // Managers
 const ExampleManager = require('./interactors/example')
 // Dependencies injection
-const exampleManager = new ExampleManager(exampleRepository)
+const exampleManager = new ExampleManager(exampleRepository, {
+  addUseCaseLogger: googleContainerLogger.getAddUseCaseContainer(),
+  getAllUseCaseLogger: googleContainerLogger.getGetAllUseCaseContainer(),
+  changeNameUseCaseLogger: googleContainerLogger.getChangeNameUseCaseContainer(),
+})
 
 /// //// PRIMARY ADAPTERS (INPUT) \\\\ \\\
 
 // Express configuration
 const ExpressApi = require('./adapters/primary/express')
 // Dependency injection
-const api = new ExpressApi(exampleManager)
+const api = new ExpressApi(exampleManager, {
+  addUseCaseLogger: googleContainerLogger.getAddUseCaseContainer(),
+  getAllUseCaseLogger: googleContainerLogger.getGetAllUseCaseContainer(),
+  changeNameUseCaseLogger: googleContainerLogger.getChangeNameUseCaseContainer(),
+  defaultLogger: googleContainerLogger.getDefaultContainer(),
+})
 
 api.start(8080)
 
