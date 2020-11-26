d55f1a9464fe993f046a95e7fd070d41ab67d050
fran.gimenez <fran.gimenez@trazable.io>
2020-11-26 18:10:23 +0100

    feat(methods): add new methods

--- src/ports/kms.js
+++ src/ports/kms.js
@@ -1,12 +1,23 @@
 class KeyManagement {
   /**
+ * Encrypts an element
+ * @async
+ * @param {string | Buffer | ArrayBuffer | Array | Array-like} plaintext
+ * @param {string} keyName any of kms.KMS_KEY
+ * @returns {Promise<string>} element digested in base64
+ */
+  async encrypt (plaintext, keyName) {
+    return Promise.reject(new Error('Method encrypt not implemented'))
+  }
+
+  /**
  *
  * @async
  * @param {string} ciphertext as base64 string
  * @returns {Promise<string>} element digested in ascii
  */
-  async decryptFile (chiperText, keyName) {
-    return Promise.reject(new Error('Method decryptFile not implemented'))
+  async decrypt (chiperText, keyName) {
+    return Promise.reject(new Error('Method decrypt not implemented'))
   }
 }
 
--- src/ports/storage.js
+++ src/ports/storage.js
@@ -1,5 +1,21 @@
 class Storage {
   /**
+  * @return {Promise<Array<Bucket>>} elements digested
+  */
+  async getBuckets () {
+    return Promise.reject(new Error('Method getBuckets not implemented'))
+  }
+
+
+  /**
+  * @param {string} bucketName
+  * @return {Promise<Array<File>>} elements digested
+  */
+  async getFiles (bucketName) {
+    return Promise.reject(new Error('Method getFiles not implemented'))
+  }
+
+  /**
    *
    * @param {string} bucket
    * @param {string} fileName
