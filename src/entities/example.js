badcad6755b4eb6d0624e363adceb1f060488f67
fran.gimenez <fran.gimenez@trazable.io>
2020-11-30 10:35:03 +0100

    feat(hobbies): add favoriteHobbies example

--- src/entities/example.js
+++ src/entities/example.js
@@ -3,13 +3,14 @@
 class Example {
   /**
    *
-   * @param {{ id: string, name: string, lastName: string, phone: number, createdAt: Date}}
+   * @param {{ id: string, name: string, lastName: string, phone: number, hobbies: <string,number>[], createdAt: Date}}
    */
-  constructor ({ id, name, lastName, phone, createdAt }) {
+  constructor ({ id, name, lastName, phone, hobbies = [], createdAt }) {
     this.id = id
     this.name = name
     this.lastName = lastName
     this.phone = phone
+    this.hobbies = hobbies
     this.createdAt = createdAt
   }
 
@@ -18,6 +19,17 @@ class Example {
    */
   get nameAndLastName () {
     return `${this.name} ${this.lastName}`
+  }
+
+  /**
+   *
+   * @return {string[]}
+   */
+  get favoriteHobbies () {
+    return this.hobbies.filter(hobby => Object.values(hobby)
+      .some(value => value === Math.max(...this.hobbies
+        .flatMap(hobby => Object.values(hobby)))))
+      .map(favoriteHobby => Object.keys(favoriteHobby))
   }
 
   /**
