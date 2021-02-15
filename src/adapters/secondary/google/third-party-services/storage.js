7a9ca3da40356f7c5b131e82b5495aa22279d325
fran.gimenez <fran.gimenez@trazable.io>
2021-02-15 12:09:16 +0100

    feat(implementation): add new methods to the google storage implementation

--- src/adapters/secondary/google/third-party-services/storage.js
+++ src/adapters/secondary/google/third-party-services/storage.js
@@ -9,6 +9,7 @@ class GoogleStorage extends Storage {
 
   /**
    * Get a list of storage buckets
+   * @async
    * @return {Promise<Array<Bucket>>} elements digested
    */
   async getBuckets() {
@@ -18,6 +19,7 @@ class GoogleStorage extends Storage {
 
   /**
    * Get a list of storage bucket's files
+   * @async
    * @param {string} bucketName
    * @return {Promise<Array<File>>} elements digested
    */
@@ -25,6 +27,15 @@ class GoogleStorage extends Storage {
     // Get files in the bucket
     const [files] = await this.storage.bucket(bucketName).getFiles()
     return files
+  }
+
+  /**
+   * @async
+   * @param {string} bucketName
+   * @return {Promise<void>}
+   */
+  async createBucket(bucketName) {
+    await this.storage.createBucket(bucketName)
   }
 
   /**
@@ -39,6 +50,60 @@ class GoogleStorage extends Storage {
     const [content] = await this.storage.bucket(bucket).file(fileName).download()
 
     return content
+  }
+
+  /**
+   * Uploads an object to a storage bucket
+   * @async
+   * @param {string} bucketName
+   */
+  async uploadObject(bucketName, fileName, serviceName) {
+    // Uploads a local file to the bucket
+    await this.storage.bucket(bucketName).upload(fileName, {
+      // Support for HTTP requests made with `Accept-Encoding: gzip`
+      // gzip: true,
+      // By setting the option `destination`, you can change the name of the
+      // object you are uploading to a bucket.
+      destination: `${serviceName}/${fileName.split('/')[3]}`,
+      metadata: {
+        // Enable long-lived HTTP caching headers
+        // Use only if the contents of the file will never change
+        // (If the contents will change, use cacheControl: 'no-cache')
+        // cacheControl: 'public, max-age=31536000',
+        cacheControl: 'no-cache',
+      },
+    })
+  }
+
+  /**
+   * Upload fileBuffer
+   * @param {strin} filePath
+   * @param {string} fileName
+   * @param {string} mimeType
+   * @param {Buffer} fileBuffer
+   * @param {string} bucketName
+   */
+  async uploadFile(fileName, mimeType, fileBuffer, bucketName) {
+    const bucket = this.storage.bucket(bucketName)
+    const file = bucket.file(fileName)
+    return new Promise((resolve, reject) => {
+      const stream = file.createWriteStream({
+        metadata: {
+          contentType: mimeType,
+        },
+        resumable: false,
+      })
+
+      stream.on('error', error => {
+        reject(error)
+      })
+
+      stream.on('finish', () => {
+        fileBuffer.cloudStorageObject = fileName
+        resolve()
+      })
+      stream.end(fileBuffer.buffer)
+    })
   }
 }
 
