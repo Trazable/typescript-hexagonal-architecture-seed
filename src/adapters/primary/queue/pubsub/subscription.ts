ec30896156474312cc0b55c4182cbae16d7a8ca9
Fran Giménez <fran.gimenez@trazable.io>
2022-06-06 12:45:07 +0200

    feat(subscriber): manage sigterm event to graceful close the subscription

--- src/adapters/primary/queue/pubsub/subscription.ts
+++ src/adapters/primary/queue/pubsub/subscription.ts
@@ -1,6 +1,5 @@
 import { Message, PubSub, Subscription as PubSubSuscription } from '@google-cloud/pubsub'
 import { ILogger } from '../../../../ports/logger'
-import { SUBSCRIPTIONS_AND_TOPICS } from '../../../../constants'
 
 /**
  * General abstraction to connect to subscriptions and execute a handler on message
@@ -9,33 +8,42 @@ export class Subscription {
   private readonly pubSubClient: PubSub
   private readonly messageHandler: (message: Message) => Promise<void>
   private readonly logger: ILogger
-  private readonly subscriptionName?: keyof typeof SUBSCRIPTIONS_AND_TOPICS
 
+  /**
+   * This property must contain at the first parameter the subscription desired and at the second parameter their topic name
+   */
+  private readonly subscriptionAndTopic: [string, string]
   constructor(
     pubSubClient: PubSub,
     messageHandler: (message: Message) => Promise<void>,
     logger: ILogger,
-    subscriptionName?: keyof typeof SUBSCRIPTIONS_AND_TOPICS
+    subscriptionAndTopic: [string, string]
   ) {
     this.pubSubClient = pubSubClient
     this.messageHandler = messageHandler
     this.logger = logger
-    this.subscriptionName = subscriptionName
+    this.subscriptionAndTopic = subscriptionAndTopic
   }
 
   async initSubscription(): Promise<void> {
-    if (this.subscriptionName) {
+    if (this.subscriptionAndTopic) {
       try {
         // Subscriptions
-        const subscription = await this.getPubSubSubscription(this.subscriptionName)
+        const subscription = await this.getPubSubSubscription(this.subscriptionAndTopic)
         // Subscription handler
         subscription.on('message', this.messageHandler)
 
         subscription.on('error', error => {
           this.logger.error(error)
         })
+
         subscription.on('close', () => {
-          this.logger.error(`Subscription ${this.subscriptionName} closed`)
+          this.logger.warn(`Subscription ${this.subscriptionAndTopic} closed`)
+        })
+
+        process.on('SIGTERM', () => {
+          this.logger.info('Closing subscription from SIGTERM received')
+          subscription.close()
         })
       } catch (error) {
         if (error instanceof Error) this.logger.error(error?.stack || error.message)
@@ -45,31 +53,26 @@ export class Subscription {
 
   /**
    *
-   * @param topicName PUBSUB TOPIC
-   * @param subscriptionName PUBSUB SUBSCRIPTION
+   * @param subscriptionAndTopic PUBSUB SUBSCRIPTION AND TOPIC
    * @returns Returns a PUBSUB subscription, if the subscription and topic doesn't exists, then are created
    */
-  private async getPubSubSubscription(
-    subscriptionName: keyof typeof SUBSCRIPTIONS_AND_TOPICS
-  ): Promise<PubSubSuscription> {
+  private async getPubSubSubscription([subscription, topic]: [string, string]): Promise<PubSubSuscription> {
     // Retrieve the PUBSUB subscription
-    const subscription = this.pubSubClient.subscription(subscriptionName)
+    const pubsubSubscription = this.pubSubClient.subscription(subscription)
 
     // Check if the subscriptions exists
-    const [exists] = await subscription.exists()
+    const [exists] = await pubsubSubscription.exists()
 
     // Return the subscription
-    if (exists) return subscription
-    // Retrieve the TOPIC name by subscription
-    const topicName = SUBSCRIPTIONS_AND_TOPICS[subscriptionName]
+    if (exists) return pubsubSubscription
 
     // Create TOPIC if not exists
-    await this.createTopicIfNotExists(topicName)
+    await this.createTopicIfNotExists(topic)
 
-    this.logger.info(`Creating subscription ${subscriptionName} to the topic ${topicName}`)
+    this.logger.info(`Creating subscription ${subscription} to the topic ${topic}`)
 
     // Create the SUBSCRIPTION
-    const [newSubscription] = await this.pubSubClient.createSubscription(topicName, subscriptionName, {
+    const [newSubscription] = await this.pubSubClient.createSubscription(topic, subscription, {
       retryPolicy: { minimumBackoff: { seconds: '10' }, maximumBackoff: { seconds: '600' } }, // Backoff delay => 10s up to 600s
       ackDeadlineSeconds: 60, // ACK Deadline 60 seconds
       messageRetentionDuration: { seconds: 604800 }, // Message retention duration => 7 days
